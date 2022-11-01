2bdd126433b5df43eabfd4d00ee5b484e5125909
vanja.atanasoski <vanja.atanasoski@valtech.com>
2022-11-01 15:25:46 +0100

    My first component is done with getData

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/VanjaContactImpl.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/VanjaContactImpl.java
@@ -1,24 +1,15 @@
 package com.adobe.aem.guides.wknd.core.models.impl;
-
-import com.adobe.aem.guides.wknd.core.models.Byline;
 import com.adobe.aem.guides.wknd.core.models.VanjaContact;
-import com.day.cq.search.QueryBuilder;
-import com.day.cq.wcm.api.Page;
-import com.day.cq.wcm.api.components.ComponentContext;
 import javax.security.auth.login.CredentialException;
-import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.Required;
-import org.apache.sling.models.annotations.injectorspecific.OSGiService;
-import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
-import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
 @Model(
-    adaptables = {SlingHttpServletRequest.class},
-    adapters = {Byline.class},
-    resourceType = {BylineImpl.RESOURCE_TYPE},
+    adaptables = {Resource.class},
+    adapters = {VanjaContact.class},
+    resourceType = {VanjaContactImpl.RESOURCE_TYPE},
     defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
 )
 public class VanjaContactImpl implements VanjaContact {
@@ -28,13 +19,10 @@ public class VanjaContactImpl implements VanjaContact {
   @ValueMapValue
   private  String title;
   @ValueMapValue
-  @Required
   private  String firstName;
   @ValueMapValue
-  @Required
   private  String lastName;
   @ValueMapValue
-  @Required
   private String phoneNumber;
   @ValueMapValue
   private  String email;
@@ -70,11 +58,11 @@ public class VanjaContactImpl implements VanjaContact {
     if (getPhoneNumber().isEmpty() && getFirstName().isEmpty() && getLastName().isEmpty()) {
     throw new CredentialException();
     } else {
-      return "The title is:" + title+
-          "\n The First Name is" +firstName+
-          "\n The Last Name is" +lastName+
-          "\n The Phonenumber is" +phoneNumber+
-          "\n The email is" +email;
+      return "The title is: " + title+
+          "<br/> The First Name is: " +firstName+
+          "<br/> The Last Name is: " +lastName+
+          "<br/> The Phonenumber is: " +phoneNumber+
+          "<br/> The email is: " +email;
     }
   }
 }
