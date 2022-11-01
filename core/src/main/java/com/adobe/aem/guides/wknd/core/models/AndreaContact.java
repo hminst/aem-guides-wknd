7909f9992537c39965db8aa2b4836b67ffe288c5
andrea.beleska <andrea.beleska@valtech.com>
2022-11-01 14:25:32 +0100

    As a AEM user I like to have a component to define contact information of a person. The component should be available on all content pages. Component Name: <your name> - Contact
    The following configuration options should be available within the component dialog:
    Title (text field, optional)
    First name (text field, required)
    Last name (text field, required)
    Phone Number (text field, required)
    E-Mail Address (Text field, optional)

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/AndreaContact.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/AndreaContact.java
@@ -4,6 +4,7 @@ package com.adobe.aem.guides.wknd.core.models;
 
 public interface AndreaContact {
 
+  String getTitle();
   String getFirstName();
   String getLastName();
   String getPhoneNumber();
--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/AndreaContactImpl.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/AndreaContactImpl.java
@@ -6,6 +6,7 @@ import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_T
 import com.adobe.aem.guides.wknd.core.models.AndreaContact;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Required;
 import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
@@ -17,9 +18,12 @@ public class AndreaContactImpl implements AndreaContact {
   protected static final String RESOURCE_TYPE = "wknd/components/andrea-beleska-contact";
 
   @ValueMapValue(name=PROPERTY_RESOURCE_TYPE, injectionStrategy=InjectionStrategy.REQUIRED)
+  protected String  title;
+  @ValueMapValue(name=PROPERTY_RESOURCE_TYPE, injectionStrategy=InjectionStrategy.REQUIRED)
   protected String  firstName;
 
-  @ValueMapValue(name="lastNameDetail")
+  @ValueMapValue
+  @Required
   private String lastName;
 
   @ValueMapValue(name=PROPERTY_RESOURCE_TYPE, injectionStrategy=InjectionStrategy.OPTIONAL)
@@ -28,6 +32,11 @@ public class AndreaContactImpl implements AndreaContact {
   @ValueMapValue(name=PROPERTY_RESOURCE_TYPE, injectionStrategy=InjectionStrategy.REQUIRED)
   private String  phoneNumber;
 
+
+  @Override
+  public String getTitle() {
+    return title;
+  }
 
   @Override
   public String getFirstName() {
