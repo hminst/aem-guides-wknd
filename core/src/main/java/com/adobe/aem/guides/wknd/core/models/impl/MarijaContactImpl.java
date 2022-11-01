caa0c7c6c661aaeac04a4ab97b3e115462674994
marija.spasovska <marija.spasovska@valtech.com>
2022-11-01 14:49:49 +0100

    feature:MS-contact-component

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/MarijaContactImpl.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/MarijaContactImpl.java
@@ -1,22 +1,10 @@
 package com.adobe.aem.guides.wknd.core.models.impl;
 
-import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_TYPE;
-
-import com.adobe.aem.guides.wknd.core.models.ImageList;
 import com.adobe.aem.guides.wknd.core.models.MarijaContact;
-import com.day.cq.search.QueryBuilder;
-import javax.annotation.Generated;
-import jdk.nashorn.internal.objects.annotations.Getter;
 import org.apache.sling.api.SlingHttpServletRequest;
-import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.Optional;
-import org.apache.sling.models.annotations.Required;
 import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
-import org.apache.sling.models.annotations.injectorspecific.OSGiService;
-import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
-import org.apache.sling.models.factory.ModelFactory;
 
 @Model(
     adaptables = {SlingHttpServletRequest.class},
@@ -25,12 +13,6 @@ import org.apache.sling.models.factory.ModelFactory;
 )
 public class MarijaContactImpl implements MarijaContact {
   protected static final String RESOURCE_TYPE = "wknd/components/marija-contact";
-  @Self
-  @Required
-  private SlingHttpServletRequest request;
-
-  @OSGiService
-  private ModelFactory modelFactory;
 
   @ValueMapValue(name="title", injectionStrategy=InjectionStrategy.OPTIONAL)
   private String title;
