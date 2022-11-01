b265cdce7cc987d8abb9e0281086e01db3dbc112
Lindit Ajvazaj <lindit.ajvazaj@valtech.com>
2022-11-01 15:02:18 +0100

    create linditcontact Component

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/LinditContactModel.java
@@ -0,0 +1,48 @@
+package com.adobe.aem.guides.wknd.core.models;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class LinditContactModel {
+
+    @ValueMapValue
+    protected String title;
+    @ValueMapValue
+    protected String firstName;
+    @ValueMapValue
+    protected String lastName;
+    @ValueMapValue
+    protected String phoneNumber;
+    @ValueMapValue
+    protected String emailAddress;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public boolean hasContent() {
+        return StringUtils.isNoneBlank(title, firstName, lastName, phoneNumber, emailAddress);
+    }
+
+}
